39a7f681bb0f570e6868a55d27bd9bb1b621b793
KekikAkademi <kekikakademi@yaani.com>
2020-03-17 01:04:17 +0300

    Qt5 Öğreniyorum Vol.1 :)

--- 14-GUI/ders_3_PyQt5_init.py
+++ 14-GUI/ders_3_PyQt5_init.py
@@ -9,19 +9,19 @@ def KekikAkademi():
     pencere = QDialog()                             # Penceremizi Oluşturduk
     pencere.setWindowTitle("Pencere Başlığı!")      # Pencere Başlığımızı Belirledik
     pencere.setGeometry(250,250,400,400)            # 250x250 kordinatında başlayarak / 400x400 ebatında aç
-    dikey = QHBoxLayout()                           # Dikey ölçekte yerleştir
+    dikey = QHBoxLayout()                           # Dikey ölçekte dinamik yerleştir
 
     buton_1 = QPushButton(pencere)                  # Pencere İçinde Buton Oluştur
     buton_1.setText("Tıklama Alanı 1")              # Buton Yazısı Tanımladık
-    dikey.addWidget(buton_1)                        # Buton 1'i Dikey Ölçekte Yerleştir
+    dikey.addWidget(buton_1)                        # Buton 1'i Dikey Ölçekte dinamik Yerleştir
     buton_1.clicked.connect(Buton1)                 # Tıklandığı zaman Buton1 Fonksiyonunu Çalıştır
 
     buton_2 = QPushButton(pencere)                  # Pencere İçinde Buton Oluştur
     buton_2.setText("Tıklama Alanı 2")              # Buton Yazısı Tanımladık
-    dikey.addWidget(buton_2)                        # Buton 2'yi Dikey Ölçekte Yerleştir
+    dikey.addWidget(buton_2)                        # Buton 2'yi Dikey Ölçekte dinamik Yerleştir
     buton_2.clicked.connect(Buton2)                 # Tıklandığı zaman Buton2 Fonksiyonunu Çalıştır
 
-    pencere.setLayout(dikey)                        # Pencereyi Dikey Ölçekte Çalıştır
+    pencere.setLayout(dikey)                        # Pencereyi Dikey Ölçekte dinamik Çalıştır
     pencere.show()                                  # Pencereyi göster
     sys.exit(uygulama.exec())                       # çıkış yapıldığı zaman, uygulamayı kapat
 
